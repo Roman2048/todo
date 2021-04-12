8efee74eb65bfbc02f79b7e89ebc2a6fb0b87274
Roman K <roman.k.person@gmail.com>
2021-04-13 01:10:48 +0300

    Added drawer

--- app/src/main/java/net/longday/planner/ui/HomeFragment.kt
+++ app/src/main/java/net/longday/planner/ui/HomeFragment.kt
@@ -3,16 +3,17 @@ package net.longday.planner.ui
 import android.content.Context
 import android.os.Bundle
 import android.view.View
-import android.view.Window
-import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.AppCompatImageButton
 import androidx.core.os.bundleOf
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -21,6 +22,7 @@ import net.longday.planner.R
 import net.longday.planner.adapter.ViewPagerAdapter
 import net.longday.planner.data.entity.Category
 import net.longday.planner.viewmodel.CategoryViewModel
+
 
 /**
  * Основной рабочий экран приложения.
@@ -34,6 +36,9 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private val categoryViewModel: CategoryViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        childFragmentManager.beginTransaction()
+//            .add(R.id.some_container, CatigoriesListFragment())
+//            .commit();
         val viewPager: ViewPager2 = view.findViewById(R.id.category_view_pager)
         val pagerAdapter = ViewPagerAdapter(this, listOf())
         viewPager.adapter = pagerAdapter
@@ -61,12 +66,21 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         categoryEditorButton.setOnClickListener {
             view.findNavController().navigate(R.id.action_homeFragment_to_categoryEditorFragment)
         }
+
+        val mDrawerLayout = requireActivity().findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val appBarButton: BottomAppBar = view.findViewById(R.id.bottom_app_bar)
+        appBarButton.setNavigationOnClickListener {
+            mDrawerLayout.openDrawer(GravityCompat.START);
+        }
+
+
     }
 
     private fun View.showKeyboard() {
         val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0)
     }
+
 }
 
 //        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
