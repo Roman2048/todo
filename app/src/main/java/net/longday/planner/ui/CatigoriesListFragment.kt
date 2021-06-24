c1c592fa06a79c8c1edaa135ab00f8f5a909104c
Roman K <roman.k.person@gmail.com>
2021-06-24 22:21:48 +0300

    Added category reordering (need big fix)

--- app/src/main/java/net/longday/planner/ui/CatigoriesListFragment.kt
+++ app/src/main/java/net/longday/planner/ui/CatigoriesListFragment.kt
@@ -13,6 +13,8 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.*
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.button.MaterialButton
@@ -35,10 +37,47 @@ import java.util.*
 @AndroidEntryPoint
 class CatigoriesListFragment : Fragment(R.layout.fragment_categories_list) {
 
+    private val itemTouchHelper by lazy {
+        // 1. Note that I am specifying all 4 directions.
+        //    Specifying START and END also allows
+        //    more organic dragging than just specifying UP and DOWN.
+        val simpleItemTouchCallback =
+            object : ItemTouchHelper.SimpleCallback(UP or
+                    DOWN or
+                    START or
+                    END, 0) {
+
+                override fun onMove(recyclerView: RecyclerView,
+                                    viewHolder: RecyclerView.ViewHolder,
+                                    target: RecyclerView.ViewHolder): Boolean {
+
+                    val adapter = recyclerView.adapter as CategoryAdapter
+                    val from = viewHolder.adapterPosition
+                    val to = target.adapterPosition
+                    // 2. Update the backing model. Custom implementation in
+                    //    MainRecyclerViewAdapter. You need to implement
+                    //    reordering of the backing model inside the method.
+//                    adapter.moveItem(from, to)
+                    // 3. Tell adapter to render the model update.
+                    adapter.notifyItemMoved(from, to)
+
+                    return true
+                }
+                override fun onSwiped(viewHolder: RecyclerView.ViewHolder,
+                                      direction: Int) {
+                    // 4. Code block for horizontal swipe.
+                    //    ItemTouchHelper handles horizontal swipe as well, but
+                    //    it is not relevant with reordering. Ignoring here.
+                }
+            }
+        ItemTouchHelper(simpleItemTouchCallback)
+    }
+
     private val categoryViewModel: CategoryViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val recycler: RecyclerView = view.findViewById(R.id.categories_recycler_view)
+        itemTouchHelper.attachToRecyclerView(recycler)
         val addCategoryItem: MaterialTextView =
             view.findViewById(R.id.categories_add_new_category_item)
         val adapter = CategoryAdapter(listOf(), requireContext())
