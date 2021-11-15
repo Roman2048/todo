e327b0fcc459886ad8df45d0a971829789056733
Roman K <roman.k.person@gmail.com>
2021-11-15 23:41:50 +0300

    Add splash screen

--- app/src/main/java/net/longday/planner/PlannerActivity.kt
+++ app/src/main/java/net/longday/planner/PlannerActivity.kt
@@ -1,7 +1,15 @@
 package net.longday.planner
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class PlannerActivity : AppCompatActivity(R.layout.activity_planner)
+class PlannerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
+        setContentView(R.layout.activity_planner)
+    }
+}
