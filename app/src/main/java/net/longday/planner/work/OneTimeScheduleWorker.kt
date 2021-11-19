541b826d90c3b021998be6a47c72c8b15ddffd4a
Roman K <roman.k.person@gmail.com>
2021-11-19 23:38:51 +0300

    Add pending intent for push

--- app/src/main/java/net/longday/planner/work/OneTimeScheduleWorker.kt
+++ app/src/main/java/net/longday/planner/work/OneTimeScheduleWorker.kt
@@ -1,12 +1,16 @@
 package net.longday.planner.work
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import net.longday.planner.PlannerActivity
 import net.longday.planner.R
 import kotlin.random.Random
+
 
 class OneTimeScheduleWorker(
     val context: Context,
@@ -14,15 +18,20 @@ class OneTimeScheduleWorker(
 ) : Worker(context, workerParams) {
 
     override fun doWork(): Result {
+        val intent = Intent(context, PlannerActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(applicationContext, 42, intent, 0)
         val builder = NotificationCompat.Builder(context, "planner_nc_1000")
 //            .setVibrate(longArrayOf(700L))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
             .setSmallIcon(R.drawable.ic_round_alarm_24)
             .setContentTitle(context.getString(R.string.notification_title))
             .setContentText(inputData.getString("content"))
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setCategory(NotificationCompat.CATEGORY_REMINDER)
         with(NotificationManagerCompat.from(context)) {
-            notify(Random.nextInt(), builder.build())
+            val randomInt = Random.nextInt()
+            notify(randomInt, builder.build())
         }
         return Result.success()
     }
